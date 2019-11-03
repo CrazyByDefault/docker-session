99a39b06daf128c42cd37dcb7887eab276b06ae8
CrazyByDefault <es16btech11025@iith.ac.in>
2019-11-03 17:20:37 +0530

    react-app populated

---
+++ react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import Graph from "./pages/Graph.jsx";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Graph />
+    </div>
+  );
+}
+
+export default App;
